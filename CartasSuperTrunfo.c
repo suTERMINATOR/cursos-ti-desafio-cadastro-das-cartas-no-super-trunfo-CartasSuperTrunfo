2fa0d8eddafe18904c31f8317457c466b3f81b54
suTERMINATOR <202601237055@alunos.estacio.br>
2026-03-11 15:04:39 +0000

    AvSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,85 @@
+
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+typedef struct {
+    char estado[50];
+    char sigla[3];
+    char cidade[50];
+    unsigned long int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+} Carta;
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    Carta c1, c2;
+    float densidade1, densidade2;
+    int opcao;
+
+    // --- Cadastro Simplificado (Exemplo Carta 1) ---
+    printf("Cidade 1: "); scanf(" %[^\n]s", c1.cidade);
+    printf("Populacao: "); scanf("%lu", &c1.populacao);
+    printf("Area (km2): "); scanf("%f", &c1.area);
+    printf("PIB (bilhoes): "); scanf("%f", &c1.pib);
+
+    // --- Cadastro Simplificado (Exemplo Carta 2) ---
+    printf("\nCidade 2: "); scanf(" %[^\n]s", c2.cidade);
+    printf("Populacao: "); scanf("%lu", &c2.populacao);
+    printf("Area (km2): "); scanf("%f", &c2.area);
+    printf("PIB (bilhoes): "); scanf("%f", &c2.pib);
+
+    // Calculos
+    densidade1 = (float)c1.populacao / c1.area;
+    densidade2 = (float)c2.populacao / c2.area;
+
+    // --- Menu de Comparação ---
+    printf("\nEscolha o atributo para comparar:\n");
+    printf("1 - Populacao (Maior vence)\n");
+    printf("2 - PIB (Maior vence)\n");
+    printf("3 - Densidade (Menor vence)\n");
+    printf("Opcao: ");
+    scanf("%d", &opcao);
+
+    printf("\n=== RESULTADO ===\n");
+
+    switch(opcao) {
+        case 1:
+            printf("Atributo: Populacao\n");
+            if (c1.populacao > c2.populacao) {
+                printf("Vencedor: %s\n", c1.cidade);
+            } else if (c2.populacao > c1.populacao) {
+                printf("Vencedor: %s\n", c2.cidade);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 2:
+            printf("Atributo: PIB\n");
+            if (c1.pib > c2.pib) {
+                printf("Vencedor: %s\n", c1.cidade);
+            } else if (c2.pib > c1.pib) {
+                printf("Vencedor: %s\n", c2.cidade);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 3:
+            printf("Atributo: Densidade Populacional\n");
+            // Lógica Inversa: Menor valor ganha
+            if (densidade1 < densidade2) {
+                printf("Vencedor: %s (Menor densidade)\n", c1.cidade);
+            } else if (densidade2 < densidade1) {
+                printf("Vencedor: %s (Menor densidade)\n", c2.cidade);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        default:
+            printf("Opcao invalida.\n");
+    }
 
     return 0;
 }
